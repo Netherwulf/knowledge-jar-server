6283071d5f94ec13ca41f023fc35b30211edfe6a
Kamil Kanclerz <kanclerz3@tlen.pl>
2018-10-27 01:05:31 +0200

    Added basic Spring Initalizr project
    Added basic Spring Initalizr project

---
+++ src/main/java/netherwulf/springframework/knowledgejar/KnowledgeJarApplication.java
@@ -0,0 +1,12 @@
+package netherwulf.springframework.knowledgejar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeJarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowledgeJarApplication.class, args);
+    }
+}
